ccefa2c584c6fb1a7fc7cbc8b827aaa54d822591
John-Dag Addy <jaedagy@gmail.com>
2022-11-13 23:14:41 +0000

    feat: implement membership upgrade screen with live data

---
+++ lib/screens/membership/details/widget_member_avatar_screen_header.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:poimen/services/cloudinary_service.dart';
+import 'package:poimen/theme.dart';
+import 'package:poimen/widgets/avatar_with_initials.dart';
+import 'package:poimen/screens/membership/models_membership.dart';
+
+class MemberAvatarScreenHeader extends StatelessWidget {
+  const MemberAvatarScreenHeader({
+    Key? key,
+    required this.member,
+    required this.memberPicture,
+  }) : super(key: key);
+
+  final MemberForList member;
+  final CloudinaryImage memberPicture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              '${member.firstName} ${member.lastName}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            Text(
+              member.status ?? member.typename,
+              style: const TextStyle(color: PoimenTheme.textSecondary),
+            )
+          ],
+        ),
+        const Padding(padding: EdgeInsets.all(10.0)),
+        Column(
+          children: [
+            AvatarWithInitials(
+              foregroundImage: memberPicture.image,
+              member: member,
+              radius: 45,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
