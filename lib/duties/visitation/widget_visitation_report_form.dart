d1f3cd50ee36ac2836bc4f00c225f48b6fdc93ee
John-Dag Addy <jaedagy@gmail.com>
2022-12-10 13:33:51 +0000

    feat: implement button and bottom modal form filling visit report

--- lib/duties/visitation/widget_visitation_report_form.dart
+++ lib/duties/visitation/widget_visitation_report_form.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:poimen/duties/imcl/gql_imcls.dart';
+import 'package:poimen/helpers/constants.dart';
 import 'package:poimen/screens/membership/models_membership.dart';
 import 'package:poimen/theme.dart';
 import 'package:poimen/widgets/alert_box.dart';
+import 'package:poimen/widgets/image_upload_button.dart';
+import 'package:location/location.dart';
 
 class OutstandingVisitationReportForm extends StatefulHookWidget {
   const OutstandingVisitationReportForm({Key? key, required this.member}) : super(key: key);
@@ -17,10 +20,18 @@ class OutstandingVisitationReportForm extends StatefulHookWidget {
 
 class _OutstandingVisitationReportFormState extends State<OutstandingVisitationReportForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _pictureUrl = '';
+
+  void setPictureUrl(String url) {
+    setState(() {
+      _pictureUrl = url;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    String reason = '';
+    String visitationReport = '';
+    var location = Location();
     final reportMutation = useMutation(
       MutationOptions(
         document: recordReasonForMemberAbsence,
@@ -47,7 +58,7 @@ class _OutstandingVisitationReportFormState extends State<OutstandingVisitationR
     return Padding(
       padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
       child: SizedBox(
-        height: 200,
+        height: 600,
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
@@ -57,13 +68,24 @@ class _OutstandingVisitationReportFormState extends State<OutstandingVisitationR
                 key: _formKey,
                 child: Column(
                   children: <Widget>[
+                    Text('${widget.member.firstName} ${widget.member.lastName}',
+                        style: PoimenTheme.heading2),
+                    Text('Visitation Report', style: PoimenTheme.heading2),
+                    const Padding(padding: EdgeInsets.all(15.0)),
+                    ImageUploadButton(
+                      preset: visitationReportPreset,
+                      setPictureUrl: setPictureUrl,
+                      child: const Text('Upload Picture'),
+                    ),
+                    const Padding(padding: EdgeInsets.all(8.0)),
                     TextFormField(
-                      maxLines: 2,
-                      decoration: InputDecoration(
-                        hintText: 'Why Was ${widget.member.firstName} Not In Church?',
+                      maxLines: 4,
+                      decoration: const InputDecoration(
+                        labelText: 'Comment',
+                        hintText: 'What is your report on this visitation?',
                       ),
                       onSaved: (String? value) {
-                        reason = value ?? '';
+                        visitationReport = value ?? '';
                       },
                       validator: (String? value) {
                         if (value == null || value.isEmpty) {
@@ -90,9 +112,13 @@ class _OutstandingVisitationReportFormState extends State<OutstandingVisitationR
                             _formKey.currentState!.save();
 
                             reportMutation.runMutation({
-                              'memberId': widget.member.id,
-                              'reason': reason,
+                              'latitude': 0.0,
+                              'longitude': 0.0,
+                              'pciture': _pictureUrl,
+                              'comment': visitationReport,
                               'roleLevel': 'Fellowship',
+                              'memberId': widget.member.id,
+                              'cycleId': 'cycle-id'
                             });
 
                             ScaffoldMessenger.of(context).showSnackBar(
