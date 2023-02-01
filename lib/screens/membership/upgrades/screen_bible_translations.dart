bc8adb33e7f7738815e48bb0fc61f442226562f1
John-Dag Addy <jaedagy@gmail.com>
2023-02-01 00:35:43 +0000

    fix: update submit flow for member upgrades

--- lib/screens/membership/upgrades/screen_bible_translations.dart
+++ lib/screens/membership/upgrades/screen_bible_translations.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:poimen/screens/membership/upgrades/gql_member_upgrades.dart';
 import 'package:poimen/screens/membership/upgrades/widget_boolean_upgrade.dart';
+import 'package:poimen/widgets/alert_box.dart';
 
 class BibleTranslationsScreen extends StatefulHookWidget {
   const BibleTranslationsScreen({Key? key}) : super(key: key);
@@ -26,8 +27,49 @@ class _BibleTranslationsScreenState extends State<BibleTranslationsScreen> {
             return;
           }
 
-          Navigator.of(context).popUntil(ModalRoute.withName('/membership-upgrades'));
+          if (resultData.isNotEmpty) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return Dialog(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    constraints: const BoxConstraints(maxHeight: 350),
+                    child: AlertBox(
+                      type: AlertType.success,
+                      title: 'Bible Translations Upgrade',
+                      message: 'Member upgraded successfully!',
+                      buttonText: 'OK',
+                      onRetry: () => // pop two screens from navigator
+                          Navigator.of(context)
+                              .popUntil(ModalRoute.withName('/membership-upgrades')),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
+        onError: (error) => showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                constraints: const BoxConstraints(maxHeight: 350),
+                child: AlertBox(
+                  type: AlertType.error,
+                  title: 'Error Submitting Bible Translations Report',
+                  message: getGQLException(error),
+                  buttonText: 'OK',
+                  onRetry: () => Navigator.of(context).pop(),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
 
--- lib/screens/membership/upgrades/screen_holy_ghost_baptism.dart
+++ lib/screens/membership/upgrades/screen_holy_ghost_baptism.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:poimen/screens/membership/upgrades/gql_member_upgrades.dart';
 import 'package:poimen/screens/membership/upgrades/widget_baptism_form.dart';
+import 'package:poimen/widgets/alert_box.dart';
 
 class HolyGhostBaptismScreen extends StatefulHookWidget {
   const HolyGhostBaptismScreen({Key? key}) : super(key: key);
@@ -26,8 +27,49 @@ class _HolyGhostBaptismScreenState extends State<HolyGhostBaptismScreen> {
             return;
           }
 
-          Navigator.of(context).popUntil(ModalRoute.withName('/membership-upgrades'));
+          if (resultData.isNotEmpty) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return Dialog(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    constraints: const BoxConstraints(maxHeight: 350),
+                    child: AlertBox(
+                      type: AlertType.success,
+                      title: 'Holy Ghost Baptism Upgrade',
+                      message: 'Member upgraded successfully!',
+                      buttonText: 'OK',
+                      onRetry: () => // pop two screens from navigator
+                          Navigator.of(context)
+                              .popUntil(ModalRoute.withName('/membership-upgrades')),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
+        onError: (error) => showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                constraints: const BoxConstraints(maxHeight: 350),
+                child: AlertBox(
+                  type: AlertType.error,
+                  title: 'Error Submitting Holy Ghost Baptism Report',
+                  message: getGQLException(error),
+                  buttonText: 'OK',
+                  onRetry: () => Navigator.of(context).pop(),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
 
